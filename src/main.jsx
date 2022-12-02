9be4e08b1c55f61e424b7310bd0482de70610e6c
Andrea Lin <andrea.lin@finalis.com>
2022-12-02 16:58:25 -0300

    feat(apollo): setup apollo provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client'
+
 import App from './components/App'
 import './styles/index.css'
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
